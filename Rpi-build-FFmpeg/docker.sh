e746e65ae0ae5ceb3b32bebf9fb406c79132c593
SignalFlagZ <Signal0Flag0Z@gmail.com>
2019-11-30 20:24:14 +0900

    change build directory

--- Rpi-build-FFmpeg/docker.sh
+++ Rpi-build-FFmpeg/docker.sh
@@ -6,6 +6,6 @@ docker build --rm=true -t buildffbase .
 docker run -it --name buildff buildffbase
 # To change ALSA version.
 # docker run -it --env V_ALSA=1.1.9 --name buildff buildffbase
-docker cp buildff:/home/ffmpeg/ffmpeg ./
-docker cp buildff:/home/ffmpeg/ffplay ./
-docker cp buildff:/home/ffmpeg/ffprobe ./
+docker cp buildff:/root/bin/ffmpeg ./
+docker cp buildff:/root/bin/ffplay ./
+docker cp buildff:/root/bin/ffprobe ./
