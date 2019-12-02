21e87437aa92901fe7ad358b0040bad2b0b341a6
SignalFlagZ <Signal0Flag0Z@gmail.com>
2019-12-02 10:58:37 +0900

    Update docker.sh

--- Rpi-build-FFmpeg/docker.sh
+++ Rpi-build-FFmpeg/docker.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # Build image and run buildFF script.
 # Signal Flag Z
-
-docker build --rm=true -t buildffbase .
-docker run -it --name buildff buildffbase
+dname=buildff
+docker build --rm=true -t ${dname} .
+if [ "$(docker ps -q -f name=${dname})" ]; then
+echo Container ${dname} is running. Stops it.
+  docker stop ${dname}
+fi
+if [ "$(docker ps -aq -f status=exited -f name=$dname)" ]; then
+echo Container ${dname} exist. Remove it.
+  docker rm ${dname}
+fi
+docker run -it --name ${dname} ${dname}
 # To change ALSA version.
-# docker run -it --env V_ALSA=1.1.9 --name buildff buildffbase
+# docker run -it --env V_ALSA=1.1.9 --name ${dname} ${dname}
 docker cp buildff:/root/bin/ffmpeg ./
 docker cp buildff:/root/bin/ffplay ./
 docker cp buildff:/root/bin/ffprobe ./
