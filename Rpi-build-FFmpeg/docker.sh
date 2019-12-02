431976aa7f84779babc1aaa1db6e505dcbe07416
SignalFlagZ <Signal0Flag0Z@gmail.com>
2019-12-02 11:07:19 +0900

    Update docker.sh

--- Rpi-build-FFmpeg/docker.sh
+++ Rpi-build-FFmpeg/docker.sh
@@ -11,7 +11,7 @@ if [ "$(docker ps -aq -f status=exited -f name=$dname)" ]; then
 echo Container ${dname} exist. Remove it.
   docker rm ${dname}
 fi
-docker run -it --name ${dname} ${dname}
+docker run -it --rm --name ${dname} ${dname}
 # To change ALSA version.
 # docker run -it --env V_ALSA=1.1.9 --name ${dname} ${dname}
 docker cp buildff:/root/bin/ffmpeg ./
